fefd970af76fa3f5f9b666db891e4b0f366ab11c
Matheus Henrique <matheus.braz@transformatechadvisors.com>
2026-03-12 14:33:11 -0300

    adicionando aula de Select/Insert

---
+++ scripts/02-select-insert.sql
@@ -0,0 +1,16 @@
+-- database: ../database/02-select-insert.sqlite
+
+CREATE TABLE cursos (
+    id INTEGER NOT NULL,
+    nome TEXT NOT NULL, 
+    aulas INTEGER
+);
+
+PRAGMA TABLE_INFO ('cursos'); 
+
+SELECT * FROM cursos;
+
+INSERT INTO cursos (id, nome, aulas) VALUES 
+(1, 'HTML', 10),
+(2, 'JS', 15),
+(3, 'CSS', 20);
