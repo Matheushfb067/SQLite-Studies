601e2b77925dd5f64862aadf807abf3161892a18
Matheus Henrique <matheus.braz@transformatechadvisors.com>
2026-03-14 00:27:31 -0300

    adicionando comandos de update - ultima aula da parte de introdução ao SQLite

---
+++ scripts/05-update.sql
@@ -0,0 +1,16 @@
+-- database: ../database/05-update.sqlite
+
+CREATE TABLE cursos (
+    id INT NOT NULL,
+    nome TEXT NOT NULL,
+    aulas INTEGER
+);
+
+INSERT INTO 
+    "cursos" ("id", "nome", "aulas")
+VALUES
+    (1, 'HTML', 10),
+    (2, 'JS', 15),
+    (3, 'CSS', 20);
+
+UPDATE "cursos" SET "nome" = 'HTML e CSS',"aulas" = 14 WHERE id = 1; -- atualiza o nome onde o id é 1
