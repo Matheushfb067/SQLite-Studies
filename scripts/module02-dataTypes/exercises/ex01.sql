6619b04275613d0109e775370d8ff593bdd2a571
Matheus Henrique <matheus.braz@transformatechadvisors.com>
2026-07-09 23:06:37 -0300

    feat: adicionando novo exercicio

---
+++ scripts/module02-dataTypes/exercises/ex01.sql
@@ -0,0 +1,89 @@
+-- database: ../../../database/module02-dataTypes/exercises/ex01.sqlite
+
+CREATE TABLE "users" (
+    "id" INTEGER PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL, 
+    "email" TEXT NOT NULL COLLATE NOCASE UNIQUE,
+    "created" TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
+)STRICT;
+
+INSERT INTO "users" ("name", "password", "email") VALUES ("Matheus", "senha123", "matheush@gmail.com");
+
+CREATE TABLE "courses" (
+    "id" INTEGER PRIMARY KEY,
+    "slug" TEXT NOT NULL COLLATE NOCASE UNIQUE,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "aulas" INTEGER NOT NULL,
+    "horas" INTEGER NOT NULL,
+    "created" TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
+)STRICT;
+
+INSERT INTO "courses" ("slug", "title", "description", "aulas", "horas") VALUES 
+(
+    'javascript-basico', 
+    'JavaScript Básico', 
+    'Aprenda os fundamentos da linguagem JavaScript.',
+    20,
+    5
+);
+
+CREATE TABLE "lessons" (
+    "id" INTEGER NOT NULL PRIMARY KEY,
+    "course_id" INTEGER NOT NULL,
+    "slug" TEXT NOT NULL COLLATE NOCASE UNIQUE,
+    "title" TEXT NOT NULL,
+    "materia" TEXT NOT NULL, 
+    "materia_slug" TEXT NOT NULL,
+    "seconds" INTEGER NOT NULL,
+    "video" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "lesson_order" INTEGER NOT NULL,
+    "free" INTEGER NOT NULL DEFAULT 0 CHECK ("free" IN (0,1)), -- 0 paga 1 gratuita
+    "created" TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    
+    FOREIGN KEY ("course_id") REFERENCES "courses" ("id"),
+    UNIQUE("course_id", "slug") -- A combinação entre curso e slug é unica!
+)STRICT;
+
+INSERT INTO "lessons" ("course_id", "slug", "title", "materia", "materia_slug", "seconds", "video", "description", "lesson_order", "free") VALUES 
+(
+    1,
+    'variaveis-let-const',
+    'Variáveis: let e const',
+    'Fundamentos',
+    'fundamentos',
+    480,
+    'variaveis.mp4',
+    'Entenda a diferença entre let, const e var.',
+    3,
+    1
+);
+
+CREATE TABLE "lessons_completed" (
+    "user_id" INTEGER NOT NULL,
+    "course_id" INTEGER NOT NULL, 
+    "lesson_id" INTEGER NOT NULL, 
+    "completed" TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("user_id", "course_id", "lesson_id"),
+
+    FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE,
+    FOREIGN KEY ("course_id") REFERENCES "courses" ("id"),
+    FOREIGN KEY ("lesson_id") REFERENCES "lessons" ("id")
+)STRICT;
+
+INSERT INTO "lessons_completed" ("user_id", "course_id", "lesson_id") VALUES (1,1,1);
+
+CREATE TABLE "certificates" (
+  "id" TEXT PRIMARY KEY,
+  "user_id" INTEGER NOT NULL,
+  "course_id" INTEGER NOT NULL,
+  "completed" TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE ("user_id", "course_id"),
+  FOREIGN KEY ("course_id") REFERENCES "courses" ("id"),
+  FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE
+)STRICT;
+
+INSERT INTO "certificates" ("id", "user_id", "course_id") VALUES ('KSFHKDSHBSDK', 1, 1);
